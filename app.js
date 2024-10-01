bc25fb99556204f88498b9c087846735c014539b
Fatima Munawar <fattimahh24@gmail.com>
2024-10-01 17:07:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+// ANS NO: 01
+let height = 170;
+let newHeight = height + 10;
+alert(newHeight);
+
+// ANS NO: 02
+let num1 = 50;
+let num2 = 30;
+let sum = num1 + num2;
+alert("The sum of num1 and num2 is: " + sum);
+
+// ANS NO: 03
+let numToBeAdded = 20;
+let total = numToBeAdded + 0;
+alert("The total is: " + total);
+
+// ANS NO: 04
+let price = 120;
+let tax = price * 0.1;
+let totalPrice = price + tax;
+alert ("the total price with tax is " + totalPrice);
+
+// ANS NO: 05
+let myAddress = "karachi";
+alert ("My address is " + myAddress);
+
+// ANS NO: 06
+let userName = "fatima";
+let userEmail = "fattimahh24@gmail.com";
+let userPassword = "xyz...123"
+alert("User Information:\nName: " + userName + "\nEmail: " + userEmail + "\nPassword: " + userPassword);
+
+// ANS NO: 07
+// let user address = "malir city, karachi";  // This will cause a syntax error
+let userAddress = "malir city, karachi";
+alert("User address is: " + userAddress);
+
+// ANS NO: 08
+let rose = "Floribundas";  // Declaring the variable with lowercase
+alert("Value of rose is: " + rose);  // This will work and display "Floribundas"
+
+// alert("Value of Rose: " + Rose);  // This will cause an error because 'Rose' is not defined
